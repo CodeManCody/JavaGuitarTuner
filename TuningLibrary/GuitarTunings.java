036a0ddd7d63f4eefa6ef1238c31073b2e09b52e
Cody <codymorgan89@gmail.com>
2018-03-23 22:27:16 -0700

    guitar lib started

---
+++ TuningLibrary/GuitarTunings.java
@@ -0,0 +1,24 @@
+
+package TuningLibrary;
+
+public class GuitarTunings
+{
+    private String[] Standard = {"E","A","D","G","B","E"};
+    private String[] DropD = {"D","A","D","G","B","E"};
+    private String[] DropC = {"C","G","C","F","A","D"};
+    
+    public String[] getStandard()
+    {
+        return Standard;
+    }
+    
+    public String[] getDropD()
+    {
+        return DropD;
+    }
+    
+    public String[] getDropC()
+    {
+        return DropC;
+    }
+}
